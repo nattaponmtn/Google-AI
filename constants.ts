8c91667ee404f1d43753fa9a5e7f78afd16ef32d
nattaponmtn <91341432+nattaponmtn@users.noreply.github.com>
2025-11-21 14:48:44 +0700

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -2,6 +2,6 @@
 // This file now only contains configuration constants and empty initial arrays
 // to provide type-safe structures before the real data is fetched from the API.
 
-export const SHEET_API_URL = "https://script.google.com/macros/s/AKfycbycMmoYQzZCV2_fHmsNF67hcmuSODjmolPmIUmkRLLkDHUDm10--f7KOz5pmwMfOn7t/exec";
+export const SHEET_API_URL = "https://script.google.com/macros/s/AKfycbwKRwQ-9HiGGnT3WITfaM8uWmX4FNpRVnKV-wg56NNkO4Prk574-WhurmDywWyiqHY5/exec";
 export const SHEET_API_KEY = "CMMS-NATTAPON-KEY-2025";
 export const GEMINI_API_KEY = process.env.API_KEY || '';
